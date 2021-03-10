1d3eabd95b1afe582af93ed8eafa6c84ae3d4937
Wembley Leach <wembley@hey.com>
2021-03-09 19:50:01 -0500

    Finish simple webserver project

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,21 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::{fs, thread};
 use std::time::Duration;
+use simple_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
